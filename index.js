8834f9a9b0be431254311845105a48c98ab64ad1
+ <+>
2023-01-16 04:45:39 -0700

    complete looping code along

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+//FOR loop
+function writeCards(obj, eventName) {
+    const nameArray = [...obj];
+    let newArray = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        //Update new array with message
+        newArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newArray;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+//WHILE Loop
+
+function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+
+
+countDown(10);
